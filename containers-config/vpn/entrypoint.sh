a450fee9909d29b4d8ae5b664e5a206c428ff41e
Aurélien Bras <aurelien.bras@gmail.com>
2024-05-03 12:11:52 +0200

    Feat/dynamic local port (#62)
    * fix: remove VPN_LOCAL_PORT from generic config to avoid conflict with vpn.env
    
    * feat: vpn ports are now dymanics
    
    * move default to VPN_LOCAL_PORT in compose to avoid rebuild docker image

--- containers-config/vpn/entrypoint.sh
+++ containers-config/vpn/entrypoint.sh
@@ -7,14 +7,14 @@ sleep 2s
 echo "Activation de l'IP ${IP_PRIV}"
 ip address add dev wg0 ${IP_PRIV}/${VPN_WILDCARD}
 echo "Activation du listen port"
-wg set wg0 listen-port 51820
+wg set wg0 listen-port ${VPN_LOCAL_PORT}
 
 echo "Activation de la clé privée"
 echo ${KEY_PRIV} > privkey
 chmod 600 privkey
 wg set wg0 private-key ./privkey
 echo "Configuration du serveur ${VPN_SERVER}"
-wg set wg0 peer ${KEY_VPN_SERVER} allowed-ips 0.0.0.0/0 endpoint ${VPN_SERVER}:51820 persistent-keepalive 25
+wg set wg0 peer ${KEY_VPN_SERVER} allowed-ips 0.0.0.0/0 endpoint ${VPN_SERVER}:${VPN_SERVER_PORT} persistent-keepalive 25
 echo "On active l'interface"
 sleep 2s
 ip link set up dev wg0
